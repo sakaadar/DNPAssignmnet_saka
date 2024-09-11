c07a08904d5d8e0aa33480bf4bbb79d0ca4884e6
Sakaadar <79919443+Sakaadar@users.noreply.github.com>
2024-09-11 23:24:01 +0200

    InMemoryRepositories

---
+++ InMemoryRepositories/PostInMemoryRepository.cs
@@ -0,0 +1,58 @@
+﻿using Entities;
+using RepositoryContracts;
+
+namespace InMemoryRepositories;
+
+public class PostInMemoryRepository: IPostRepository
+{
+    private List<Post> posts;
+    
+    public Task<Post> AddAsync(Post post)
+    {
+       post.Id = posts.Any() ? posts.Max(p => p.Id) + 1 : 1;
+       posts.Add(post);
+       return Task.FromResult(post);
+    }
+
+    public Task UpdateAsync(Post post)
+    {
+       Post? existingPost = posts.SingleOrDefault(p=>p.Id==post.Id);
+       if (existingPost == null)
+       {
+           throw new InvalidOperationException(
+               $"Post with ID '{post.Id}' does not exist");
+       }
+       posts.Remove(existingPost);
+       posts.Add(post);
+       return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        Post? postToRemove = posts.SingleOrDefault(p => p.Id == id);
+        if (postToRemove is null)
+        {
+            throw new InvalidOperationException(
+                $"Post with ID '{id}' does not exist");
+        }
+
+        posts.Remove(postToRemove);
+        return Task.CompletedTask;
+    }
+
+    public Task<Post> GetSingleAsync(int id)
+    {
+        Post? SinglePost = posts.SingleOrDefault(p => p.Id == id);
+        if (SinglePost is null)
+        {
+            throw new InvalidOperationException(
+                $"Post with ID '{id}' does not exist");
+        }
+        return Task.FromResult(SinglePost);
+    }
+
+    public IQueryable<Post> GetMany()
+    {
+        return posts.AsQueryable();
+    }
+}
